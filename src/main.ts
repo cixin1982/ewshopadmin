eb0ec4a7589e277ce5af68121dafe08595baa7f2
cx <cx@zi>
2023-02-16 14:24:54 +0800

    登录页面

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+
 import '@/styles/tailwind.css'
-import '@/styles/index.less'
+import '@/styles/index.css'
 
 const app = createApp(App)
 //创建并挂载根实例
